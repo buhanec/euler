43619205ade9440c3a52797f0d099c6c90b9ecca
Alen Buhanec <alen.buhanec@gmail.com>
2017-08-06 19:44:42 +0100

    pep257 fixes

--- 001.py
+++ 001.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
-"""Find the sum of all the multiple of 3 or 5 below 1000."""
+"""
+Project Euler #1.
+
+Find the sum of all the multiple of 3 or 5 below 1000.
+"""
 
 
 print(sum(n for n in range(1000) if not n % 3 or not n % 5))
